d32ed268ea1d40a74d9fdfa6d089c13962c00772
stestr8 <stephon@hq.newdream.net>
2010-11-25 16:27:29 -0800

    Remove unused tag line

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -56,7 +56,6 @@ tags = {
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
 	tags[s] = awful.tag(tags.names, s, tags.layouts);
-    --tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, default_layouts[s])
 end
 -- }}}
 
